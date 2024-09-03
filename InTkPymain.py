c19d46a7b567385c51f21216873d11e5c01825f3
KelvinVieira <kelvinvieirabut2@gmail.com>
2024-09-02 21:50:31 -0300

    Update InTkPymain.py
    Comentários.

--- InTkPymain.py
+++ InTkPymain.py
@@ -4,32 +4,32 @@ from tkinter import ttk
 from tkinter import filedialog
 import subprocess
 
-# Janela principal(com menus acessando todas as outras aplicações), janela login
 
-def abrirUser():
-    subprocess.Popen(['python', 'InTkPy.py'])
-    root.destroy()
+def abrirUser():  # Método para a abertura da aplicação usuários
+    subprocess.Popen(['python', 'InTkPy.py'])  # É usada o módulo subprocess, que abre, e executa o arquivo indicado
+    root.destroy()  # Após abertura do outro arquivo, é destruída a janela(arquivo) atual
 
-def abrirCid():
+def abrirCid():  # Método para a abertura da aplicação cidades
     subprocess.Popen(['python', 'InTkPy2.py'])
     root.destroy()
 
-def abrirCli():
+def abrirCli():  # Método para a abertura da aplicação clientes
     subprocess.Popen(['python', 'InTkPy3.py'])
     root.destroy()
 
 root = Tk()
 root.title("main")
 
-menubar = Menu(root)
+menubar = Menu(root)  # Criação do objeto menu pelo construtor Menu, com indicação de root em seu parâmetro
 
-root.config(menu=menubar)
-interface = Menu(menubar)
-menubar.add_cascade(label='Cadastros', menu=interface)
+# Criação e configuração de menu
+root.config(menu=menubar)  # Configurando em root, a variável menubar como um menu
+interface = Menu(menubar)  # Variável interface recebe construtor Menu com todas as configurações de menubar
+menubar.add_cascade(label='Cadastros', menu=interface) # Adciciona ao menu menubar um item menu com opção Cadastros
 
 # root.state("zoomed")
-interface.add_command(label='Usuário', command=abrirUser)
-interface.add_separator()
+interface.add_command(label='Usuário', command=abrirUser)  # É adicionado um item comando em interface, com nome, e o comando a ser executado
+interface.add_separator()  # Adiciona um separador
 interface.add_command(label='Cidade', command=abrirCid)
 interface.add_separator()
 interface.add_command(label='Clientes', command=abrirCli)
