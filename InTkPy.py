29eb10c7ace7e6400c1f67157012a50a7a672da1
KelvinVieira <kelvinvieirabut2@gmail.com>
2024-09-11 21:50:20 -0300

    Update InTkPy.py
    Pequenos ajustes.

--- InTkPy.py
+++ InTkPy.py
@@ -1,21 +1,21 @@
-# Execução final da aplicação, e criação de todos os widgets para interface
-from usuIUDS import Usuarios  # Importando arquivo classe de manutenção IUDS(Insert, Update, Delete, Select)
-from db import db  # Importando arquivo classe de conexão ao banco de dados
-from tkinter import *  # Importando classes Tkinter
+from usuIUDS import Usuarios  # Execução final da aplicação, e criação de todos os widgets para interface
+from db import db
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 import subprocess
 
-
-class Application:  # Classe principal
+banco = db()
+c = banco.cnxao.cursor()
+class Application:
     def __init__(self, master=None):
-        self.fonte = ("Verdana", "8")  # Configuração de uma fonte
+        self.fonte = ("Verdana", "8")
 
-        self.container1 = Frame(master)  # Criação de um widget...
-        self.container1["pady"] = 10  # com suas configurações
-        self.container1.pack()  # e finalizando com seu gerenciador de geometria, que decide sua posição em relação a um widget
+        self.container1 = Frame(master)
+        self.container1["pady"] = 10
+        self.container1.pack()
 
         self.container2 = Frame(master)
         self.container2["padx"] = 20
@@ -56,18 +56,18 @@ class Application:  # Classe principal
         self.container9["pady"] = 15
         self.container9.pack()
 
-        self.titulo = Label(self.container1, text="Informe os dados :")  # Texto em widget 1
-        self.titulo["font"] = ("Calibri", "9", "bold")  # Com fonte definida
+        self.titulo = Label(self.container1, text="Informe os dados :")
+        self.titulo["font"] = ("Calibri", "9", "bold")
         self.titulo.pack()
 
         self.lblidusuario = Label(self.container2, text="idUsuario:", font=self.fonte, width=10)
         self.lblidusuario.pack(side=LEFT)
-        self.txtidusuario = Entry(self.container2)  # Já em widget 2, além do texto acima, é criado e configurado um campo de inserção de dados para o usuário
+        self.txtidusuario = Entry(self.container2)
         self.txtidusuario["width"] = 14
         self.txtidusuario["font"] = self.fonte
         self.txtidusuario.pack(side=LEFT)
-        self.btnBuscar = Button(self.container2, text="Buscar", font=self.fonte, width=10)  # Com um botão para busca de informações de acordo com a informação provida pelo usuário
-        self.btnBuscar["command"] = self.buscarUsuario  # Ao ser acionado, o botão realiza o comando, que por sua vez, é um método
+        self.btnBuscar = Button(self.container2, text="Buscar", font=self.fonte, width=10)
+        self.btnBuscar["command"] = self.buscarUsuario
         self.btnBuscar.pack(side=RIGHT)
 
         self.lblnome = Label(self.container3, text="Nome:", font=self.fonte, width=10)
@@ -128,18 +128,18 @@ class Application:  # Classe principal
 
         # Configurando a Treeview
         columns = ("ID", "Nome", "Telefone", "E-mail", "Usuário","Senha")  # Definindo as colunas
-        self.treeview = ttk.Treeview(root, columns=columns, show='headings')  # Criação e configuração da Treeview
+        self.treeview = ttk.Treeview(root, columns=columns, show='headings')
         for col in columns:
-            self.treeview.heading(col, text=col)  # Criação das colunas por meio de um laço
-        self.treeview.bind("<<TreeviewSelect>>", self.buscarUsuarioTree)  # Ao selecionar algum item da Treeview, o método de busca é acionado
+            self.treeview.heading(col, text=col)
+        self.treeview.bind("<<TreeviewSelect>>", self.buscarUsuarioTree)
         self.treeview.pack(fill=tk.BOTH, expand=True)
 
         # Pegando os dados do banco e populando a Treeview
-        data = fetch_data()  # Por meio do método fetch_data, a variável criada data recebe todos os valores buscados pelo método
-        populate_treeview(self.treeview, data)  # E os atribui a outro método para a população desta Treeview
+        data = fetch_data()
+        populate_treeview(self.treeview, data)
 
-    def inserirUsuario(self):  # Método de inserção
-        user = Usuarios()  # Criação de objeto user a partir do construtor de outro arquivo Usuarios
+    def inserirUsuario(self):
+        user = Usuarios()
 
         user.nome = self.txtnome.get()
         user.telefone = self.txttelefone.get()
@@ -156,11 +156,11 @@ class Application:  # Classe principal
         self.txtusuario.delete(0, END)
         self.txtsenha.delete(0, END)
 
-        messagebox.showinfo(  # Uso de messagebox como meio de comunicação ao usuário
+        messagebox.showinfo(
             message=f"Valores inseridos com sucesso!", title="Inserção"
         )
 
-    def alterarUsuario(self):  # Método de alteração
+    def alterarUsuario(self):
         user = Usuarios()
 
         user.idusuario = self.txtidusuario.get()
@@ -183,7 +183,7 @@ class Application:  # Classe principal
             message=f"Valores alterados com sucesso!", title="Alteração"
         )
 
-    def excluirUsuario(self):  # Método de exclusão
+    def excluirUsuario(self):
         user = Usuarios()
 
         user.idusuario = self.txtidusuario.get()
@@ -201,7 +201,7 @@ class Application:  # Classe principal
             message=f"Valores excluídos com sucesso.", title="Exclusão"
         )
 
-    def buscarUsuario(self):  # Método de busca
+    def buscarUsuario(self):
         user = Usuarios()
 
         idusuario = self.txtidusuario.get()
@@ -220,9 +220,9 @@ class Application:  # Classe principal
         self.txtsenha.delete(0, END)
         self.txtsenha.insert(INSERT,user.senha)
 
-    def buscarUsuarioTree(self, event):  # Método de busca da Treeview
-        seleciona_item = self.treeview.selection()  # Obtém da Treeview o valor selecionado, e o atribui a variável criada
-        if seleciona_item:  # Se selecionar item (não estiver null)
+    def buscarUsuarioTree(self, event):
+        seleciona_item = self.treeview.selection()
+        if seleciona_item:
             # Obtém o item selecionado
             item = seleciona_item[0]
             values = self.treeview.item(item, 'values')
@@ -240,11 +240,11 @@ class Application:  # Classe principal
             self.txtsenha.delete(0, END)
             self.txtsenha.insert(INSERT, values[5])
 
-def abrirMain():  # Método já fora da classe principal para ações do menu
-    subprocess.Popen(['python', 'InTkPymain.py'])  # Este em específico, usa módulo subprocess para abrir outro arquivo, e executá-lo
-    root.destroy()  # Fechamento da janela atual
+def abrirMain():
+    subprocess.Popen(['python', 'InTkPymain.py'])
+    root.destroy()
 
-def fetch_data():  # Método de coleta de dados do banco para uso na Treeview
+def fetch_data():
     # Conectando ao banco de dados
     banco = db()
     c = banco.cnxao.cursor()
@@ -256,24 +256,21 @@ def fetch_data():  # Método de coleta de dados do banco para uso na Treeview
 
     return rows
 
-# Função para popular a Treeview com os dados do banco
+    # Função para popular a Treeview com os dados do banco
 def populate_treeview(treeview, data):
     for row in data:
         treeview.insert("", "end", values=row)
 
-
-# Definindo características da janela principal root
 root = Tk()
-root.title("Usuários")  # Título
-menubar = Menu(root)  # Criação da variável responsável por receber o Menu com características root
+root.title("Usuários")
+menubar = Menu(root)
 
-# Criação e configuração de menu
-root.config(menu=menubar)  # Configurando em root, a variável menubar como um menu
-interface = Menu(menubar)  # Variável interface recebe menu com todas as configurações de menubar
-menubar.add_cascade(label='Páginas', menu=interface)  # Adciciona ao menu menubar um item menu com opção Páginas
+root.config(menu=menubar)
+interface = Menu(menubar)
+menubar.add_cascade(label='Páginas', menu=interface)
 
-root.state("zoomed")  # root preenche a tela(tela-cheia)
-interface.add_command(label='Principal', command=abrirMain)  # É adicionado um item comando em interface, com nome, e o comando a ser executado
+root.state("zoomed")
+interface.add_command(label='Principal', command=abrirMain)
 
 # Iniciando aplicação
 Application(root)
